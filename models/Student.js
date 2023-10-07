1f27e47c7e440903d459cc32960d811e241e5819
ZylaCode <zyla.verena@gmail.com>
2023-10-07 17:16:02 +0200

    validation added

--- models/Student.js
+++ models/Student.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
-
+import pkg from "validator";
+const {isEmail} = pkg;
 // name (string)
 // first_name (string)
 // email (string)
@@ -7,17 +8,26 @@ import mongoose from "mongoose";
 const StudentSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: [true, "Please enter your name"],
+        minLength: 3,
+        maxLength: 25
     },
     first_name: {
         type: String,
-        required: true
+        required: [true, "Please enter your first name"],
+        minLength: 3,
+        maxLength: 25
     },
     email: {
         type: String,
-        required: true
+        required: [true, "Please enter your email address"],
+        minLength: 6,
+        maxLength: 35,
+        unique: true,
+        validate: [isEmail, "Please enter a valid email address"]
     }
-})
+});
+
 
 const Student = mongoose.model('Student', StudentSchema);
 
--- routes/students.js
+++ routes/students.js
@@ -2,6 +2,21 @@ import express from 'express';
 import Student from '../models/Student.js';
 const studentsRouter = express.Router();
 
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+    let errors = { name: "", first_name: "", email: ""};
+    if (err.code === 11000){
+        errors.email = "This email address is already in use";
+        return errors;
+    }
+    if (err.message.includes('user validation failed')){
+        Object.values(err.errors).forEach(({properties}) => {
+        errors[properties.path] = properties.message;        
+    })
+    }
+    return errors;
+}
+
 studentsRouter.get("/", async (req, res) => {
     try {
       const response = await Student.find();
@@ -18,20 +33,21 @@ studentsRouter.post("/", async (req, res) => {
       const response = await Student.create({name, first_name, email});
       res.json(response);
     } catch(err){
-        console.log(err)
-        res.status(500).json(err)
+        const errors = handleErrors(err);        
+        res.status(400).json({errors})
     }
 });
 
 // Using the code from the previous exercise, if the user sends a PUT query, modify all the entries with the name equal to John, change it to “Bob”.
 studentsRouter.put("/", async (req, res) => {
     try {
+        const idName = req.body;
         const {name, first_name, email} = req.body;
-        const response = await Student.updateMany({name: "John"}, {name: name, first_name: first_name, email: email});
+        const response = await Student.updateMany({name: idName}, {name, first_name, email});
         res.json(response)
     } catch(err){
-        //console.log(err)
-        res.status(500).json(err)
+        const errors = handleErrors(err);
+        res.status(500).json({errors});
     }
 });
 
@@ -43,9 +59,22 @@ studentsRouter.get("/:id", async(req, res)=> {
       res.json(response)
       //res.send('Hello')
     } catch(err){
-        res.status(500).json(err)
+        res.status(500).json(err);
     }
 });
+
+//update by :id
+studentsRouter.put("/:id", async (req, res) => {
+    try {
+        const {name, first_name, email} = req.body;
+        const {id} = req.params;
+        const response = await Student.findByIdAndUpdate(id, {name, first_name, email});
+        res.json(response)
+    } catch(err){
+        const errors = handleErrors(err);
+        res.status(500).json({errors});
+    }
+})
 
 
 //delete by :id
